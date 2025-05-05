0ac694a9ef218d9f206c124380c8ae8d859190ec
Johnny Owayed <johnny.owayed@gmail.com>
2025-05-05 10:34:00 +0300

    Initial Commit

---
+++ Mangomolo/MangomoloApp.swift
@@ -0,0 +1,17 @@
+//
+//  MangomoloApp.swift
+//  Mangomolo
+//
+//  Created by Johnny Owayed on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct MangomoloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
